2f0ff9e2232ff0e17fefe1ae96130726699a96f5
Pedro <pedromarques391@gmail.com>
2025-03-12 17:40:30 -0300

    feat: Criaçao do componente Header.

---
+++ src/components/template/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = (): React.JSX.Element => {
+    return (
+        <header className='bg-black text-white py-5 flex justify-center in-checked:'>
+            kdkdkdkdkdkd
+        </header>
+    )
+}
