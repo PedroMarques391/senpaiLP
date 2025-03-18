35ea8d5fdef3e660dc755b08d4d89f68e0fc67e5
Pedro Marques <pedromarques391@gmail.com>
2025-03-18 11:35:46 -0300

    feat: Adicionando animação spring  no componente SpeechBubble.tsx.

--- src/components/ui/SpeechBubble.tsx
+++ src/components/ui/SpeechBubble.tsx
@@ -1,4 +1,7 @@
-import { ReactNode, } from 'react'
+"use client"
+
+import { ReactNode, useRef } from 'react'
+import { motion, useInView } from "motion/react"
 type TSide = "left" | "right"
 
 interface ISpeechBubble {
@@ -8,9 +11,17 @@ interface ISpeechBubble {
 }
 
 const SpeechBubble = ({ side = "right", children, style }: ISpeechBubble): React.JSX.Element => {
+    const ref = useRef(null)
+    const isInView = useInView(ref)
+
     return (
-        <div className={`${side === "left" ? "speechBubble-left" : "speechBubble"}  h-min z-10 ${style}`}>
-            <p className='text-slate-300'>{children}</p>
+        <div className={`${side === "left" ? "speechBubble-left" : "speechBubble"}  h-min z-10 ${style} `}>
+            <motion.p
+                ref={ref}
+                initial={{ y: "100%" }}
+                animate={{ y: isInView ? 0 : "100%" }}
+                transition={{ type: "spring", stiffness: 100, damping: 30 }}
+                className='text-slate-300'>{children}</motion.p>
         </div>
     )
 }
