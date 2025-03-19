76f8734686525bcdc05a450da9bec5d4b582e6e1
Pedro Marques <pedromarques391@gmail.com>
2025-03-19 17:57:03 -0300

    fix adicionando os id nas sections corretas.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,19 @@ import senpai from "../../public/images/6.webp"
 import Image from "next/image";
 import Services from "@/components/Services";
 import { faq, functionsCardItens, services } from "@/data/utils"
-import * as React from "react"
 import CarouselBenefit from "@/components/Carousel";
 import { Faq } from "@/components/Faq";
-import { motion, useInView } from "motion/react"
-import { useRef } from "react";
-
 
 
 
 export default function Home() {
-  const ref = useRef(null)
-  const isInView = useInView(ref)
 
   return (
     <Layout hero>
-      <motion.h1
-        ref={ref}
-        initial={{ x: "-100%" }}
-        animate={{ x: isInView ? 0 : "-100%" }}
-        transition={{ type: "spring", stiffness: 100, damping: 25 }}
-        className="py-10 text-2xl text-center lg:text-left md:text-3xl">O que a senpai pode fazer por você hoje?</motion.h1>
+      <div className="w-full p-4 overflow-hidden">
+        <h1 className="py-10 text-2xl text-center lg:text-left md:text-3xl">
+          O que a senpai pode fazer por você hoje?</h1>
+      </div>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl w-[80%] md:w-full mx-auto">
         {functionsCardItens.map((functionsCardItem, item) => (
           <FunctionsCard
@@ -51,7 +43,9 @@ export default function Home() {
           />
           <h1 className="text-center text-4xl md:text-left md:text-5xl font-semibold text-black md:pb-6">Conheça nossos planos</h1>
         </div>
-        <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 md:mt-20 place-items-center gap-5 mx-auto">
+        <div
+          id="plans"
+          className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 md:mt-20 place-items-center gap-5 mx-auto">
           {services.map((service, index) => (
             <Services
               key={index}
