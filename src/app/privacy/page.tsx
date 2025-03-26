f6c0907681145724dc9b7f3a5c2e2ec44481b9ec
Pedro Marques <pedromarques391@gmail.com>
2025-03-26 18:43:34 -0300

    feat: Criando rota para politicas de privacidade.

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,39 @@
+import { privacyPolicy } from "@/data/utils";
+import Link from "next/link";
+
+
+
+const Privacy = () => {
+    return (
+        <main className="min-h-screen bg-gray-100 text-black px-6 md:px-16 py-12 flex items-center justify-center">
+            <div className="max-w-4xl bg-white p-10 rounded-2xl shadow-lg border border-gray-300">
+                <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-800 mb-6">
+                    Política de Privacidade
+                </h1>
+
+                <section className="space-y-6 divide-y divide-gray-200">
+                    {privacyPolicy.map((section, index) => (
+                        <div key={index} className="py-4">
+                            <h2 className="text-xl font-semibold text-gray-700 mb-2">{section.title}</h2>
+                            <p className="text-gray-600 leading-relaxed">{section.text}</p>
+                        </div>
+                    ))}
+                </section>
+
+                <div className="text-center mt-8">
+                    <p className="text-gray-600">Em caso de dúvidas, entre em contato:</p>
+                    <Link
+                        href="mailto:contato@botdosenpai.com.br"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-teal-500 font-semibold hover:text-teal-600 transition-colors"
+                    >
+                        contato@botdosenpai.com.br
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Privacy;
