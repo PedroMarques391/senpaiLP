9f2caae354860ae5e6436f8cf1c5b2fa558a31b8
Pedro Marques <pedromarques391@gmail.com>
2025-03-26 14:05:50 -0300

    fix: corrigindo a responsividade do Logo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,16 +3,16 @@ import senpai from "../../public/images/IconSenpai.png"
 
 const Logo = (): React.JSX.Element => {
     return (
-        <div className="flex justify-center items-center gap-2">
+        <div className="flex md:flex-col lg:flex-row justify-center items-center gap-2 md:gap-0 lg:gap-2">
             <Image
                 src={senpai}
-                alt="Foto da Senpai/Logo"
+                alt="Foto da Senpai - Logo"
                 priority
                 width={60}
                 height={60}
                 className="bg-gradient-to-r from-fuchsia-300 via-purple-300 to-slate-50 rounded-full"
             />
-            <p className="lg:text-xl font-semibold text-white">Senpai</p>
+            <p className="lg:text-xl font-semibold text-white md:-mt-1 lg:mt-0">Senpai</p>
 
         </div >
     )
