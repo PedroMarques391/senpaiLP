91ace3d2ecc0cdc5dd6b781a2f60a0ae148da77f
Pedro Marques <pedromarques391@gmail.com>
2025-03-26 01:46:09 -0300

    fix: Corrigindo width em Navitems.tsx.

--- src/components/template/header/NavItem.tsx
+++ src/components/template/header/NavItem.tsx
@@ -13,11 +13,11 @@ export const NavItem = ({ children, href, ...props }: INavItemsProps): React.JSX
         <li
             {...props}
 
-            className={`text-center w-32 md:w-auto lg:w-32 relative group px-2 `}>
+            className={`text-center w-32 md:w-auto relative group px-3 `}>
             <Link
 
                 href={href}
-                className=" w-full h-full block py-4  bg-transparent hover:bg-transparent text-base lg:text-lg"
+                className=" w-full h-full block py-4 bg-transparent hover:bg-transparent text-base lg:text-lg text-center"
             >
                 {children}
                 <p className={`absolute bottom-0 left-0 group-hover:w-full group-hover:bg-white h-1 bg-white transition-all duration-300 isCurrentPath hover:w-full  w-0`} />
