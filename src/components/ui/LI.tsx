df4db2c6820e95fae67687dfb812e3433266e301
Pedro Marques <pedromarques391@gmail.com>
2025-03-26 22:09:04 -0300

    refactor: refatorando o componente LI.tsx.

--- src/components/ui/LI.tsx
+++ src/components/ui/LI.tsx
@@ -1,13 +1,27 @@
 
+import Link from 'next/link'
 import React from 'react'
 
 interface ILIProps {
     children: React.ReactNode
+    alt: string
+    href: string
+    target?: "_blank" | "_self"
 }
 
-const LI = ({ children }: ILIProps): React.JSX.Element => {
+const LI = ({ children, alt, href, target = "_self" }: ILIProps): React.JSX.Element => {
     return (
-        <li className='p-2 w-full text-center flex justify-center items-center'>{children}</li>
+        <li className='p-2 w-full text-center flex justify-center items-center'>
+            <Link
+                href={href}
+                target={target}
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white transition duration-300"
+                aria-label={alt}
+            >
+                {children}
+            </Link>
+        </li>
     )
 }
 
