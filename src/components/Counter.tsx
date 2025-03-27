17e4cd5d57c0bbb4bbb14d3a1f98efb70b5ea49c
Pedro Marques <pedromarques391@gmail.com>
2025-03-27 01:53:11 -0300

    feat: Adicionando contagem de acordo com a visualização em Counter.tsx.

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,6 @@
 "use  client"
-import { useTransform, useMotionValue, motion, animate } from "motion/react"
-import { useEffect } from "react"
+import { useTransform, useMotionValue, motion, animate, useInView } from "motion/react"
+import { useEffect, useRef } from "react"
 
 interface ICounterProps {
     number: number
@@ -8,16 +8,24 @@ interface ICounterProps {
 }
 
 const Counter = ({ number, type }: ICounterProps): React.JSX.Element => {
+    const ref = useRef(null)
+    const isInView = useInView(ref)
     const count = useMotionValue(0)
     const rounded = useTransform(() => Math.round(count.get()))
 
     useEffect(() => {
-        const controls = animate(count, number, { duration: 5 })
-        return () => controls.stop()
-    }, [])
+        console.log(isInView)
+        if (isInView) {
+            const controls = animate(count, number, { duration: 5 })
+            return () => controls.stop()
+        }
+        return () => count.set(0)
+    }, [isInView, count, number])
 
     return (
-        <div className="flex justify-center items-center gap-2 font-sans">
+        <div
+            ref={ref}
+            className="flex justify-center items-center gap-2 font-sans">
             <motion.p>
                 {rounded}
             </motion.p>
