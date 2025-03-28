c3895f96011756d2c9b99b70feff652c406c3bed
Pedro Marques <pedromarques391@gmail.com>
2025-03-27 23:12:03 -0300

    fix: Corrigindo a responsividade para o card.

--- src/components/FunctionsCard.tsx
+++ src/components/FunctionsCard.tsx
@@ -8,24 +8,23 @@ interface IFunctionsCardProps {
     title: string
     subtitle: string
     message: string
-    duration: number
 }
 
-const FunctionsCard = ({ imagePath, title, subtitle, duration }: IFunctionsCardProps): React.JSX.Element => {
+const FunctionsCard = ({ imagePath, title, subtitle }: IFunctionsCardProps): React.JSX.Element => {
     const ref = useRef(null)
     const isInView = useInView(ref, { amount: "some" })
 
     return (
         <div
             id="func"
-            className="bg-white shadow-lg w-full flex flex-col gap-4 rounded-2xl justify-start overflow-hidden transform transition-transform hover:scale-105">
+            className="bg-white shadow-lg w-full flex flex-col gap-4 rounded-2xl justify-start overflow-hidden">
             <motion.div
                 ref={ref}
                 initial={{ x: "110%" }}
                 animate={{ x: isInView ? 0 : "110%" }}
                 transition={{
                     type: "tween",
-                    duration: duration,
+                    duration: 1.5,
                     ease: "easeInOut"
                 }}
                 className="mx-auto  w-full text-center  rounded-lg">
@@ -40,7 +39,7 @@ const FunctionsCard = ({ imagePath, title, subtitle, duration }: IFunctionsCardP
                     className="inline-block w-full object-contain h-full rounded-ss-xl"
                 />
             </motion.div>
-            <div className="p-4 h-52 space-y-4">
+            <div className="p-4 h-auto md:h-52 lg:h-60 xl:h-52 space-y-4 ">
                 <motion.h1
                     ref={ref}
                     initial={{ x: "-100%" }}
@@ -54,8 +53,8 @@ const FunctionsCard = ({ imagePath, title, subtitle, duration }: IFunctionsCardP
                     initial={{ x: "-100%" }}
                     animate={{ x: isInView ? 0 : "-100%" }}
                     transition={{ type: "spring", stiffness: 50, damping: 25, duration: 2.0 }}
-                    className="text-lg text-gray-400 grow">{
-                        subtitle}
+                    className="text-lg text-gray-400 h-full">
+                    {subtitle}
                 </motion.p>
             </div>
 
