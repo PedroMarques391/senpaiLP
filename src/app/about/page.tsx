297902937905a39b83605185f74e04d904fb7267
Pedro Marques <pedromarques391@gmail.com>
2025-03-28 20:19:51 -0300

    feat: padronizando página about.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,17 @@
+import Layout from '@/components/template/Layout'
 import React from 'react'
 
 const AboutPage = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            Site em produção
-        </div>
+        <Layout
+            hero={false}
+            backgroundColor='bg-gray-100'
+            backgroundHeader='bg-black'
+        >
+            <div className="flex justify-center items-center h-screen">
+                Site em produção
+            </div>
+        </Layout>
     )
 }
 
