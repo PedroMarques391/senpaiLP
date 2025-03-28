b9478a57ae1e3e8c4bb450bfa75fdc8b12f80348
Pedro Marques <pedromarques391@gmail.com>
2025-03-28 14:42:42 -0300

    feat: adicionando useEffect para funcionamento perfeito do efeito Deep Link Scroll Handler.

--- src/components/template/header/NavItem.tsx
+++ src/components/template/header/NavItem.tsx
@@ -1,6 +1,6 @@
-
+"use client"
 import Link from 'next/link'
-import React, { DetailedHTMLProps, LiHTMLAttributes } from 'react'
+import React, { DetailedHTMLProps, LiHTMLAttributes, useEffect } from 'react'
 
 interface INavItemsProps extends DetailedHTMLProps<LiHTMLAttributes<HTMLLIElement>, HTMLLIElement> {
     href: string
@@ -9,13 +9,25 @@ interface INavItemsProps extends DetailedHTMLProps<LiHTMLAttributes<HTMLLIElemen
 
 export const NavItem = ({ children, href, ...props }: INavItemsProps): React.JSX.Element => {
 
+    useEffect(() => {
+        const hash = window.location.hash
+        if (hash) {
+            const el = document.querySelector(hash)
+            if (el) {
+                setTimeout(() => {
+                    el.scrollIntoView({ behavior: 'smooth' })
+                }, 100)
+            }
+        }
+    }, [])
+
     return (
         <li
             {...props}
 
             className={`text-center w-32 md:w-auto relative group px-3 `}>
             <Link
-
+                scroll={true}
                 href={href}
                 className=" w-full h-full block py-4 bg-transparent hover:bg-transparent text-base lg:text-lg text-center"
             >
