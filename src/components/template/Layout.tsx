4bc6cb7859408a34525cbd3a83863ded7938db66
Pedro Marques <pedromarques391@gmail.com>
2025-03-28 17:17:56 -0300

    feat: Adicionando buttonToTop em todos os layouts.

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -4,6 +4,7 @@ import { Header } from './header/Header'
 import Hero from '../Hero'
 import { Footer } from './Footer'
 import { LoadingPage } from '../LoadingPage'
+import { ArrowUp } from 'lucide-react'
 
 
 interface ILayoutProps {
@@ -15,6 +16,7 @@ interface ILayoutProps {
 
 const Layout = ({ children, hero, backgroundColor = "bg-white", backgroundHeader = "bg-transparent" }: ILayoutProps): React.JSX.Element => {
     const [loading, setLoading] = useState<boolean>(true);
+    const [showButton, setShowButton] = useState<boolean>(false);
 
     useEffect(() => {
         const loadingTimeout = setTimeout(() => {
@@ -23,15 +25,33 @@ const Layout = ({ children, hero, backgroundColor = "bg-white", backgroundHeader
 
         return () => clearTimeout(loadingTimeout);
     }, []);
+
+    useEffect(() => {
+        const handleScroll = (): void => {
+            setShowButton(window.scrollY > 150);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [])
     return (
         <>
             {loading ? <LoadingPage /> : (
-                <main className={`w-full min-h-screen ${backgroundColor}`}>
+                <main className={`w-full min-h-screen ${backgroundColor} relative`}>
                     {hero ? <Hero /> : <Header background={backgroundHeader} />}
                     <section className='container mx-auto py-5 px-3 overflow-hidden '>
                         {children}
                     </section>
                     <Footer />
+
+
+                    <button
+                        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                        className={`bg-white text-gray-800 bottom-10 right-5 lg:right-12 fixed h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 rounded-full flex items-center justify-center shadow-lg hover:shadow-xl transition-opacity duration-500 transform hover:scale-110 z-30 ${showButton ? "opacity-100" : "opacity-0"}`} >
+                        <ArrowUp className="h-6 w-6 md:h-8 md:w-8 " />
+                    </button>
+
                 </main>
             )}
         </>
