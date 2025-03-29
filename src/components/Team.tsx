1dd917afb83b688e8a6b2d9c3c5743890ee7e6c2
Pedro Marques <pedromarques391@gmail.com>
2025-03-29 11:34:43 -0300

    style: finalizando responsividade em cards.

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -20,8 +20,8 @@ export interface ITeamProps {
 const Team = ({ image, name, jobTitle, bio, links }: ITeamProps): React.JSX.Element => {
     return (
         <section
-            className='bg-white rounded-3xl px-6 py-5 md:py-10 flex w-4/5 md:w-full flex-col sm:flex-row md:flex-col lg:flex-row md:bg-amber-600 lg:bg-red-500 justify-self-center justify-center md:items-center'>
-            <div className="w-full h-36  overflow-hidden mr-4 place-items-center ">
+            className='bg-white rounded-3xl px-6 py-5 sm:py-10 md:py-5 lg:py-10 flex w-4/5 md:w-full flex-col sm:flex-row md:flex-col lg:flex-row  justify-self-center justify-center md:items-center'>
+            <div className="w-full sm:w-36 h-36 overflow-hidden mr-4 place-items-center">
                 <Image
                     src={image}
                     alt="Foto"
@@ -31,11 +31,11 @@ const Team = ({ image, name, jobTitle, bio, links }: ITeamProps): React.JSX.Elem
                     className="object-cover h-full w-36 rounded-full"
                 />
             </div>
-            <div className='flex-1 flex flex-col items-start pl-2 gap-y-2'>
-                <h1 className='font-bold text-xl'>{name}</h1>
-                <h2 className='text-lg tracking-wider font-mono'>{jobTitle}</h2>
-                <p className='text-justify hyphens-none break-words'>{bio}</p>
-                <ul className="flex gap-x-4 mt-2">
+            <div className='flex-1 flex flex-col items-stretch sm:pl-2 md:pl-0 lg:pl-2 gap-y-1 mt-1 sm:mt-0 md:mt-1 lg:mt-0'>
+                <h1 className='font-bold text-xl w-full text-left py-2'>{name}</h1>
+                <h2 className='text-lg tracking-wider font-mono w-full text-left'>{jobTitle}</h2>
+                <p className='text-justify hyphens-none break-words  py-2'>{bio}</p>
+                <ul className="flex gap-x-4 mt-2 py-2  w-full">
                     {links.map((link, index) => (
                         <li key={index}>
                             <Link
@@ -55,3 +55,4 @@ const Team = ({ image, name, jobTitle, bio, links }: ITeamProps): React.JSX.Elem
 }
 
 export default Team
+
