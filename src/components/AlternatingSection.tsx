8dbd62fa69c13bad2d6859854b556fb981e11d3c
Pedro Marques <pedromarques391@gmail.com>
2025-03-29 15:13:05 -0300

    animate: adicionado animações em AlternatingSection.

--- src/components/AlternatingSection.tsx
+++ src/components/AlternatingSection.tsx
@@ -1,4 +1,7 @@
+"use client"
 import Image, { StaticImageData } from "next/image"
+import { useInView, motion } from "motion/react"
+import { useRef } from "react"
 
 
 interface IAlternatingSectionProps {
@@ -9,9 +12,16 @@ interface IAlternatingSectionProps {
 }
 
 const AlternatingSection = ({ image, title, children, imageGridAlign }: IAlternatingSectionProps): React.JSX.Element => {
+    const ref = useRef(null)
+    const isInView = useInView(ref)
     return (
-        <section className="grid grid-cols-1 md:grid-cols-2  place-content-center place-items-center">
-            <div className={`flex justify-center items-center bg-black w-full h-full max-h-[500px] min-h-[400px] ${imageGridAlign === "left" ? "order-2 md:order-1" : "order-1 md:order-2"}`}>
+        <section className="grid grid-cols-1 md:grid-cols-2  place-content-center place-items-center overflow-hidden">
+            <motion.div
+                ref={ref}
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ type: "tween", duration: 1, ease: "easeInOut" }}
+                className={`flex justify-center items-center bg-black w-full overflow-hidden h-full max-h-[500px] min-h-[400px] ${imageGridAlign === "left" ? "order-2 md:order-1" : "order-1 md:order-2"}`}>
                 <Image
                     alt={`Imagem ilustrativa da seção: ${title}`}
                     src={image}
@@ -20,15 +30,25 @@ const AlternatingSection = ({ image, title, children, imageGridAlign }: IAlterna
                     priority
                     className='object-cover xl:object-cover w-full h-full'
                 />
-            </div>
+            </motion.div>
             <div className={`w-full flex flex-col md:flex-row justify-start items-center p-7 h-full ${imageGridAlign === "left" ? "order-2" : "order-1"}`}>
                 <div className='w-full lg:max-w-4/5 '>
-                    <h1 className='text-xl lg:text-3xl xl:text-4xl font-bold text-gray-800 mb-4 w-full text-left '>
+                    <motion.h1
+                        initial={{ x: imageGridAlign === "left" ? "120%" : "-120%" }}
+                        animate={isInView ? { x: 0 } : { x: imageGridAlign === "left" ? "120%" : "-120%" }}
+                        transition={{ type: "tween", duration: 1, ease: "easeInOut" }}
+                        className='text-xl lg:text-3xl xl:text-4xl font-bold text-gray-800 mb-4 w-full text-left '
+                    >
                         {title}
-                    </h1>
-                    <p className='text-base lg:text-base xl:text-lg text-gray-600 leading-relaxed text-justify break-words hyphens-auto'>
+                    </motion.h1>
+                    <motion.p
+                        initial={{ x: imageGridAlign === "left" ? "100%" : "-100%" }}
+                        animate={isInView ? { x: 0 } : { x: imageGridAlign === "left" ? "120%" : "-120%" }}
+                        transition={{ type: "tween", duration: 1.5, ease: "easeInOut" }}
+                        className='text-base lg:text-base xl:text-lg text-gray-600 leading-relaxed text-justify break-words hyphens-auto'
+                    >
                         {children}
-                    </p>
+                    </motion.p>
                 </div>
             </div>
         </section>
