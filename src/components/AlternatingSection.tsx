8f3da6c4eaa6120b18c91ddb84ef4212ccfaee3f
Pedro Marques <pedromarques391@gmail.com>
2025-03-29 01:50:01 -0300

    fix: corrigindo responsividade das imagens.

--- src/components/AlternatingSection.tsx
+++ src/components/AlternatingSection.tsx
@@ -11,14 +11,14 @@ interface IAlternatingSectionProps {
 const AlternatingSection = ({ image, title, children, imageGridAlign }: IAlternatingSectionProps): React.JSX.Element => {
     return (
         <section className="grid grid-cols-1 md:grid-cols-2  place-content-center place-items-center">
-            <div className={`flex justify-center items-center bg-black w-full h-full ${imageGridAlign === "left" ? "order-2 md:order-1" : "order-1 md:order-2"}`}>
+            <div className={`flex justify-center items-center bg-black w-full h-full max-h-[500px] ${imageGridAlign === "left" ? "order-2 md:order-1" : "order-1 md:order-2"}`}>
                 <Image
-                    alt='Foto da senpai na página about.'
+                    alt={`Imagem ilustrativa da seção: ${title}`}
                     src={image}
                     height={700}
                     width={700}
                     priority
-                    className='bg-contain bg-no-repeat max-w-full h-auto'
+                    className='object-contain w-full h-full'
                 />
             </div>
             <div className={`w-full flex flex-col md:flex-row justify-start items-center p-7 h-full ${imageGridAlign === "left" ? "order-2" : "order-1"}`}>
