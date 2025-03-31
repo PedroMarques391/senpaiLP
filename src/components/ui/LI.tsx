e78f690dfcc9bdc88c0ca3ab4276e876b2535b1a
Pedro Marques <pedromarques391@gmail.com>
2025-03-31 18:43:41 -0300

    feat: adicionando pre-render condicional em LI.

--- src/components/ui/LI.tsx
+++ src/components/ui/LI.tsx
@@ -7,16 +7,18 @@ interface ILIProps {
     alt: string
     href: string
     target?: "_blank" | "_self"
+    prefetch?: boolean
 }
 
-const LI = ({ children, alt, href, target = "_self" }: ILIProps): React.JSX.Element => {
+const LI = ({ children, alt, href, target = "_self", prefetch = true }: ILIProps): React.JSX.Element => {
     return (
         <li className='p-2 w-full text-center flex justify-center items-center'>
             <Link
                 href={href}
                 target={target}
+                prefetch={prefetch}
                 rel="noopener noreferrer"
-                className="text-gray-400 hover:text-white transition duration-300"
+                className="text-gray-400 hover:text-white transition duration-300 w-full"
                 aria-label={alt}
             >
                 {children}
