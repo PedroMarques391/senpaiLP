efab641b2445d893de28c3da583d121d78be7f9a
Pedro Marques <pedromarques391@gmail.com>
2025-03-31 18:43:12 -0300

    style: melhorando a disposição dos links.

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -10,25 +10,30 @@ export const Footer = () => {
     return (
         <footer className="bg-black text-white py-16">
             <div className="container mx-auto px-4">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-16 mx-auto place-items-center">
-                    <div className="flex flex-col justify-center items-center sm:items-start">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-12 md:gap-16 mx-auto place-items-center">
+                    <div className="flex flex-col justify-center items-center sm:items-start md:col-span-1">
                         <Logo />
-                        <h1 className="text-xl sm:text-2xl font-semibold mb-4">Sobre Nós</h1>
-                        <p className="text-center sm:text-left text-gray-400 text-sm sm:text-base">
-                            Informações sobre nossa empresa e missão. Comprometidos com qualidade e inovação.
-                        </p>
                     </div>
-                    <div className="flex flex-col items-center sm:items-start">
+                    <div className="flex flex-col items-center sm:items-start md:col-span-2">
                         <h1 className="text-xl sm:text-2xl font-semibold mb-4 text-center w-full">Links Rápidos</h1>
-                        <ul className="space-y-1 w-full">
-                            <LI href={isHomePage ? "#home" : "/#home"} alt="scrool para o inicio do site.">
+                        <ul className="grid grid-cols-2 w-full ">
+                            <LI
+                                prefetch={isHomePage ? false : true}
+                                href={isHomePage ? "#home" : "/#home"} alt="scrool para o inicio do site.">
                                 Home
                             </LI>
-                            <LI href={isHomePage ? "#func" : "/#func"} alt="Scroll direto para os cards de serviços.">
+                            <LI
+                                prefetch={isHomePage ? false : true}
+                                href={isHomePage ? "#func" : "/#func"} alt="Scroll direto para os cards de serviços.">
                                 Services
                             </LI>
-                            <LI href={isHomePage ? "#plans" : "/#plans"} alt="Scroll que leva para os planos disponveis.">
+                            <LI
+                                prefetch={isHomePage ? false : true}
+                                href={isHomePage ? "#plans" : "/#plans"} alt="Scroll que leva para os planos disponveis.">
                                 Planos
+                            </LI>
+                            <LI href="/about" target="_blank" alt="link para os termos de uso do bot">
+                                Sobre Nós
                             </LI>
                             <LI href="/termo-de-uso" target="_blank" alt="link para os termos de uso do bot">
                                 Termos de Uso
@@ -38,8 +43,8 @@ export const Footer = () => {
                             </LI>
                         </ul>
                     </div>
-                    <div className="flex flex-col items-center justify-center gap-y-2">
-                        <h1 className="text-xl sm:text-2xl font-semibold mb-4 w-full">Siga-nos</h1>
+                    <div className="flex flex-col items-center justify-center gap-y-2 md:col-span-1 w-full">
+                        <h1 className="text-xl sm:text-2xl font-semibold mb-4 w-full text-center">Siga-nos</h1>
                         <ul className="w-full mx-auto space-y-1">
                             <LI
                                 target="_blank"
