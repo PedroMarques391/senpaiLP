cca57048e15c697c8b0ab10de036a1cdf4858ffc
Pedro Marques <pedromarques391@gmail.com>
2025-04-07 15:03:01 -0300

    style: justificando texto em Team.tsx.

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -33,7 +33,7 @@ const Team = ({ image, name, jobTitle, bio, links }: ITeamProps): React.JSX.Elem
                 scale: { type: "spring", visualDuration: 1, bounce: 0.2 },
             }
             }
-            className='bg-white rounded-3xl px-6 py-5 sm:py-10 md:py-5  flex w-4/5 md:w-full flex-col sm:flex-row md:flex-col lg:flex-row  justify-self-center justify-center md:items-center'>
+            className='bg-white rounded-xl px-6 py-5 sm:py-10 md:py-5  flex w-4/5 md:w-full flex-col sm:flex-row md:flex-col lg:flex-row justify-self-center justify-center md:items-center'>
             <div className="w-full sm:w-36 h-36 overflow-hidden mr-4 place-items-center">
                 <Image
                     src={image}
@@ -47,8 +47,8 @@ const Team = ({ image, name, jobTitle, bio, links }: ITeamProps): React.JSX.Elem
             <div className='flex-1 flex flex-col items-stretch gap-y-1 mt-1 sm:mt-0 md:mt-1 lg:mt-0 h-full'>
                 <h1 className='font-bold text-xl w-full text-left py-2'>{name}</h1>
                 <h2 className='text-lg tracking-wider font-mono w-full text-left '>{jobTitle}</h2>
-                <p className='text-justify hyphens-none break-words py-2 flex-1'>{bio}</p>
-                <ul className="flex gap-x-4 mt-2 py-2 w-full bg-">
+                <p className='text-justify hyphens-auto break-words py-2 flex-1'>{bio}</p>
+                <ul className="flex gap-x-4 mt-2 py-2 w-full">
                     {links.map((link, index) => (
                         <li key={index}>
                             <Link
