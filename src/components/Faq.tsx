d8efddb11c50c42529920a800b6a4c1b34bda311
Pedro Marques <pedromarques391@gmail.com>
2025-04-07 18:06:26 -0300

    fix: descolando o componente faq da tela nas versões mobile.

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -14,7 +14,7 @@ interface IFaqProps {
 export const Faq = ({ faq }: IFaqProps): React.JSX.Element => {
     return (
         <Accordion
-            type="single" collapsible className="w-full max-w-4xl mx-auto mb-10">
+            type="single" collapsible className="w-full max-w-4xl mx-auto mb-10 px-3">
             {faq.map((faqItem, key) => (
                 <AccordionItem value={`item-${key}`} key={key} >
                     <AccordionTrigger className="text-xl ">{faqItem.question}</AccordionTrigger>
