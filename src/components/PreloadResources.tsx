95618c2c0ad7f519d35769f139d6d82871517e44
Pedro Marques <pedromarques391@gmail.com>
2025-04-07 23:21:13 -0300

    feat: adicionando mais um preload em  PreloadResources.tsx.

--- src/components/PreloadResources.tsx
+++ src/components/PreloadResources.tsx
@@ -1,15 +1,17 @@
 'use client'
 
-import { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 export function PreloadResources() {
-    useEffect(() => {
-        ReactDOM.preload('/images/senpai/hero.webp', {
-            as: 'image',
-            type: 'image/webp',
-        })
-    }, [])
+    ReactDOM.preload('/images/senpai/hero.webp', {
+        as: 'image',
+        type: 'image/webp',
+    })
+
+    ReactDOM.preload('/images/about/wallpaper.webp', {
+        as: 'image',
+        type: 'image/webp',
+    })
 
     return null
 }
