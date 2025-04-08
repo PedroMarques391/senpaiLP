c1382cc88af83aa3d46626fce8aadc25c299652e
Pedro Marques <pedromarques391@gmail.com>
2025-04-08 00:01:13 -0300

    feat: adicionando sitemap.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://botdosenpai.com.br/',
+            lastModified: "2025-04-08T05:50:59+01:00",
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://botdosenpai.com.br/about',
+            lastModified: "2025-04-08T05:50:59+01:00",
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://botdosenpai.com.br/termo-de-uso',
+            lastModified: "2025-04-08T05:50:59+01:00",
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://botdosenpai.com.br/privacidade',
+            lastModified: "2025-04-08T05:50:59+01:00",
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+    ]
+}
