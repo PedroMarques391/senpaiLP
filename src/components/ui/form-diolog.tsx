9075b3c362435680ea2054087e832884e7758951
Pedro Marques <pedromarques391@gmail.com>
2025-04-10 18:38:17 -0300

    feat: criando estado para reação visual quando o email for enviado com sucesso.

--- src/components/ui/form-diolog.tsx
+++ src/components/ui/form-diolog.tsx
@@ -5,9 +5,10 @@ import { Label } from './label'
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { DialogFooter } from './dialog'
+import { DialogFooter, DialogClose } from './dialog'
 import { Button } from './button'
 import { insertMaskInPhone } from '@/src/utils'
+import { CircleCheckBig } from 'lucide-react'
 
 
 
@@ -26,6 +27,8 @@ type FormData = z.infer<typeof userSchema>
 
 const FormDialog = (): React.JSX.Element => {
     const [loading, setLoaging] = useState<boolean>(false);
+    const [emailSend, setEmailSend] = useState<boolean>(false);
+
     const { register, handleSubmit, formState: { errors }, setValue, clearErrors, reset } = useForm({
         resolver: zodResolver(userSchema),
     })
@@ -39,7 +42,7 @@ const FormDialog = (): React.JSX.Element => {
             },
             body: JSON.stringify(data)
         })
-            .then(() => console.log("foi"))
+            .then(() => setEmailSend((prev) => !prev))
             .catch(() => console.log("deu o carai"))
             .finally(() => setLoaging((prev) => !prev))
 
@@ -54,63 +57,83 @@ const FormDialog = (): React.JSX.Element => {
     }
 
     return (
-        <form onSubmit={handleSubmit(userSubmit)} className="space-y-3">
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">Nome</Label>
-                <Input className="col-span-3" {...register("name")} />
-                {errors.name && <p className="text-red-500 col-span-4 text-sm">{errors.name.message}</p>}
-            </div>
+        <>
+            {emailSend ? (
+                <div className="flex flex-col items-center justify-center h-full text-center">
+                    <CircleCheckBig size={160} color='#00ff33' />
+                    <p className="text-sm text-zinc-500 max-w-xs py-2">
+                        Obrigado por entrar em contato. Sua mensagem chegou direitinho e logo logo responderemos com todo carinho! 💌
+                    </p>
+                    <div className="w-full mt-4 flex justify-end items-center gap-2">
+                        <Button onClick={() => setEmailSend((prev) => !prev)} variant="default">
+                            Nova mensagem
+                        </Button>
+                        <DialogClose asChild>
+                            <Button variant="destructive">Sair</Button>
+                        </DialogClose>
+                    </div>
+                </div>
 
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="email" className="text-right">E-mail</Label>
-                <Input type='email' className="col-span-3" {...register("email")} />
-                {errors.email && <p className="text-red-500 col-span-4 text-sm">{errors.email.message}</p>}
-            </div>
+            ) : (
+                <form onSubmit={handleSubmit(userSubmit)} className="space-y-3">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">Nome</Label >
+                        <Input className="col-span-3" {...register("name")} />
+                        {errors.name && <p className="text-red-500 col-span-4 text-sm">{errors.name.message}</p>}
+                    </div >
 
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="phone" className="text-right">Telefone</Label>
-                <Input type="text"
-                    className="col-span-3"
-                    {...register("phone")}
-                    maxLength={15}
-                    onChange={handleMask} />
-                {errors.phone && <p className="text-red-500 col-span-4 text-sm">{errors.phone.message}</p>}
-            </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="email" className="text-right">E-mail</Label>
+                        <Input type='email' className="col-span-3" {...register("email")} />
+                        {errors.email && <p className="text-red-500 col-span-4 text-sm">{errors.email.message}</p>}
+                    </div>
 
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="subject" className="text-right">Assunto</Label>
-                <Input
-                    className="col-span-3" {...register("subject")} />
-                {errors.subject && <p className="text-red-500 col-span-4 text-sm">{errors.subject.message}</p>}
-            </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="phone" className="text-right">Telefone</Label>
+                        <Input type="text"
+                            className="col-span-3"
+                            {...register("phone")}
+                            maxLength={15}
+                            onChange={handleMask} />
+                        {errors.phone && <p className="text-red-500 col-span-4 text-sm">{errors.phone.message}</p>}
+                    </div>
 
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="message" className="text-right">Mensagem</Label>
-                <Textarea
-                    className="col-span-3 resize-none" {...register("message")} />
-                {errors.message && <p className="text-red-500 col-span-4 text-sm">{errors.message.message}</p>}
-            </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="subject" className="text-right">Assunto</Label>
+                        <Input
+                            className="col-span-3" {...register("subject")} />
+                        {errors.subject && <p className="text-red-500 col-span-4 text-sm">{errors.subject.message}</p>}
+                    </div>
 
-            <DialogFooter>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="message" className="text-right">Mensagem</Label>
+                        <Textarea
+                            className="col-span-3 resize-none" {...register("message")} />
+                        {errors.message && <p className="text-red-500 col-span-4 text-sm">{errors.message.message}</p>}
+                    </div>
 
-                <Button
-                    type="submit"
-                    disabled={loading}
-                    className={`w-full flex items-center justify-center gap-2 transition-all duration-300 ${loading ? 'bg-gray-400 text-black cursor-not-allowed' : 'bg-black hover:bg-black/80 text-white'
-                        } rounded-2xl py-3 text-base font-semibold `}
-                >
-                    {loading ? (
-                        <>
-                            <span
-                                className="inline-block h-4 w-4 border-2 border-black border-t-transparent rounded-full animate-spin" />
-                            <span>Enviando...</span>
-                        </>
-                    ) : (
-                        <span>Enviar 💌</span>
-                    )}
-                </Button>
-            </DialogFooter>
-        </form>
+                    <DialogFooter>
+
+                        <Button
+                            type="submit"
+                            disabled={loading}
+                            className={`w-full flex items-center justify-center gap-2 transition-all duration-300 ${loading ? 'bg-gray-400 text-black cursor-not-allowed' : 'bg-black hover:bg-black/80 text-white'
+                                } rounded-2xl py-3 text-base font-semibold `}
+                        >
+                            {loading ? (
+                                <>
+                                    <span
+                                        className="inline-block h-4 w-4 border-2 border-black border-t-transparent rounded-full animate-spin" />
+                                    <span>Enviando...</span>
+                                </>
+                            ) : (
+                                <span>Enviar 💌</span>
+                            )}
+                        </Button>
+                    </DialogFooter>
+                </form >
+            )}
+        </>
     )
 }
 
