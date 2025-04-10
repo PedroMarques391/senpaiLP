3bcc0c7eba079f49547c2dcc63c0172c6704737d
Pedro Marques <pedromarques391@gmail.com>
2025-04-10 10:33:07 -0300

    rename: adicionando controllers.

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { sendEmail } from './utils/mail';
+import { sendEmail } from './controller/mail.controller';
 
 export async function POST(req: NextRequest) {
     try {
