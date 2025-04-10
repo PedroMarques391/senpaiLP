83dfdc9245330388460d99eded928537297468fd
Pedro Marques <pedromarques391@gmail.com>
2025-04-10 10:24:02 -0300

    feat: criando rota para requisição POST para envio de email.

---
+++ src/app/api/emails/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { sendEmail } from './utils/mail';
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        console.log(body)
+
+        await sendEmail(body);
+
+        return NextResponse.json({ status: 200 });
+    } catch (error) {
+        console.error('Erro ao enviar email:', error);
+        return NextResponse.json(
+            { status: 500 }
+        );
+    }
+}
