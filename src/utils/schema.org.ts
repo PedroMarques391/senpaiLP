ce87a3ae62cc1d5a47dcd3bbbd0564ded5b9c17a
Pedro Marques <pedromarques391@gmail.com>
2025-04-10 21:28:18 -0300

    feat: criando schema.org para todas as rotas.

--- src/utils/schema.org.ts
+++ src/utils/schema.org.ts
@@ -1,4 +1,4 @@
-import { SoftwareApplication, WithContext } from "schema-dts";
+import { AboutPage, SoftwareApplication, WebPage, WithContext } from "schema-dts";
 
 const jsonLd: WithContext<SoftwareApplication> = {
     "@context": "https://schema.org",
@@ -20,6 +20,44 @@ const jsonLd: WithContext<SoftwareApplication> = {
     datePublished: "2025-03-25",
 }
 
+
+const jsonLdAbout: WithContext<AboutPage> = {
+    "@context": "https://schema.org",
+    "@type": "AboutPage",
+    name: "BotDoSenpai - Sobre Nós",
+    url: "https://botdosenpai.com.br/about",
+    description: "Conheça o time por trás do Bot do Senpai e a missão de espalhar fofura no WhatsApp com figurinhas personalizadas.",
+    mainEntity: {
+        "@type": "Organization",
+        name: "BotDoSenpai",
+        url: "https://botdosenpai.com.br",
+    },
+};
+
+
+const jsonLdPrivacy: WithContext<WebPage> = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: "BotDoSenpai - Política de Privacidade",
+    description: "Saiba como o Bot do Senpai coleta, armazena e protege seus dados com carinho e responsabilidade.",
+    url: "https://botdosenpai.com.br/privacidade",
+};
+
+
+const jsonLdTerms: WithContext<WebPage> = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: "BotDoSenpai - Termos de Uso",
+    url: "https://botdosenpai.com.br/termo-de-uso",
+    description: "Leia os termos e condições para uso do Bot do Senpai e descubra como usamos magia e tecnologia para servir você com segurança.",
+};
+
+
+
+
 export {
-    jsonLd
+    jsonLd,
+    jsonLdAbout,
+    jsonLdPrivacy,
+    jsonLdTerms
 }
