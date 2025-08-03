e8437e4db6744dfb5a805d111f7a77b9de779716
Pedro Marques <pedromarques391@gmail.com>
2025-08-03 20:47:18 -0300

    feat: edit hero layout in Hero.tsx.

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,8 +1,6 @@
 "use client"
 import { BsWhatsapp } from "react-icons/bs"
 import { achievements } from "@/src/data"
-import Image from "next/image"
-import whatsDemo from "@/public/images/senpai/phone.webp"
 import { motion, useInView } from "motion/react";
 import { useRef } from "react"
 import Counter from "../sections/Counter"
@@ -16,8 +14,6 @@ const Hero = (): React.JSX.Element => {
     const ref = useRef(null)
     const isInView = useInView(ref)
 
-    const imageRef = useRef(null)
-    const isInImageInView = useInView(imageRef)
 
     const refCard = useRef(null);
     const isCardInView = useInView(refCard);
@@ -27,11 +23,11 @@ const Hero = (): React.JSX.Element => {
             <div
                 className="relative bg-[url('/images/senpai/hero.webp')] bg-cover text-white bg-no-repeat bg-gradient-to-r from-fuchsia-300 via-purple-300 to-slate-50 bg-center pb-20  md:px-20 rounded-b-[60px]">
                 <Header />
-                <div className="absolute inset-0 bg-black/30 rounded-b-[60px]"></div>
+                <div className="absolute inset-0 bg-black/50 rounded-b-[60px]"></div>
                 <section
                     id="home"
-                    className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 items-center mx-auto py-10 md:pt-32 overflow-hidden">
-                    <div className="flex flex-col gap-6 z-10 px-5 md:px-0 xl:px-5 pt-16 lg:col-span-2 overflow-hidden w-full">
+                    className="container relative  gap-10 items-center mx-auto py-10 md:pt-32 overflow-hidden justify-center ">
+                    <div className="flex flex-col gap-6 z-10 px-5 md:px-0 xl:px-5 pt-16 lg:col-span-2 overflow-hidden w-full h-[700px]  justify-center items-center">
                         <motion.h1
                             ref={ref}
                             initial={{ x: "100%" }}
@@ -45,51 +41,37 @@ const Hero = (): React.JSX.Element => {
                             initial={{ x: "-100%" }}
                             animate={{ x: isInView ? 0 : "-100%" }}
                             transition={{ type: "spring", stiffness: 50, damping: 20, duration: 2 }}
-                            className="text-lg md:text-base lg:text-xl">
+                            className="text-lg md:text-base lg:text-xl w-[80%]">
 
                             A <span className="font-semibold">Senpai</span> está sempre disponível para deixar suas conversas mais divertidas e criativas! Com ela, você pode criar figurinhas personalizadas, transformar imagens, GIFs e vídeos em figurinhas animadas e muito mais. Tudo de forma rápida, fácil e gratuita. A sua bot de figurinhas para WhatsApp que torna cada conversa única!
                         </motion.h2>
-                        <Link
-                            href="https://wa.me/555497153068?text=Ol%C3%A1%20senpai"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <button className="bg-[#25D366] text-white font-bold py-2 px-3 rounded-lg shadow-md transition flex items-center gap-4 w-full md:w-fit duration-300 sm:py-1 sm:px-2 sm:gap-2 md:py-2 md:px-3 md:gap-4 hover:bg-[#1DA851] hover:scale-105 hover:shadow-lg active:scale-95 justify-center">
-                                <BsWhatsapp aria-label="Icone do WhatsApp" className="flex-shrink-0 w-5 h-5 md:w7 md:h-7" />
+                        <div className="flex gap-10">
+                            <Link
+                                href="https://wa.me/555497153068?text=Ol%C3%A1%20senpai"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <button className="bg-[#25D366] text-white font-bold py-2 px-3 rounded-lg shadow-md transition flex items-center gap-4 w-full md:w-fit duration-300 sm:py-1 sm:px-2 sm:gap-2 md:py-2 md:px-3 md:gap-4 hover:bg-[#1DA851] hover:scale-105 hover:shadow-lg active:scale-95 justify-center">
+                                    <BsWhatsapp aria-label="Icone do WhatsApp" className="flex-shrink-0 w-5 h-5 md:w7 md:h-7" />
 
-                                <div className="text-left">
-                                    <p className="text-xs">Começar no</p>
-                                    <p className="text-base font-semibolda lg:text-[17px]">WhatsApp</p>
-                                </div>
-                            </button>
+                                    <div className="text-left">
+                                        <p className="text-xs">Começar no</p>
+                                        <p className="text-base font-semibolda lg:text-[17px]">WhatsApp</p>
+                                    </div>
+                                </button>
 
-                        </Link>
-                        <p className="text-white flex items-center justify-center md:justify-normal text-sm sm:text-xs md:text-sm gap-1 sm:gap-0 flex-wrap ">
-                            Ao clicar no botão, você concorda com os&nbsp;
-                            <Link href="/termo-de-uso" prefetch target="_blank" className="text-[#25D366] font-semibold hover:underline whitespace-nowrap">
-                                Termos de Uso
-                            </Link> &nbsp;e&nbsp;
-                            <Link href="/privacidade" target="_blank" prefetch className="text-[#25D366] font-semibold hover:underline whitespace-nowrap">
-                                Política de Privacidade.
                             </Link>
-                        </p>
-
+                            <p className="text-white font-bold flex items-center justify-center md:justify-normal text-sm sm:text-xs md:text-sm gap-1 sm:gap-0 flex-wrap ">
+                                Ao clicar no botão, você concorda com os&nbsp;
+                                <Link href="/termo-de-uso" prefetch target="_blank" className="text-[#25D366] font-semibold hover:underline whitespace-nowrap">
+                                    Termos de Uso
+                                </Link> &nbsp;e&nbsp;
+                                <Link href="/privacidade" target="_blank" prefetch className="text-[#25D366] font-semibold hover:underline whitespace-nowrap">
+                                    Política de Privacidade.
+                                </Link>
+                            </p>
+                        </div>
                     </div>
-                    <motion.div
-                        ref={imageRef}
-                        initial={{ x: "100%" }}
-                        animate={{ x: isInImageInView ? 0 : "100%" }}
-                        transition={{ type: "spring", stiffness: 100, damping: 25, duration: 0.8 }}
-                        className="w-full h-full flex justify-center items-center z-10 lg:col-span-1">
-                        <Image
-                            src={whatsDemo}
-                            priority
-                            alt="Mockup WhatsApp"
-                            className="w-auto h-auto max-w-full max-h-full object-contain"
-                        />
-                    </motion.div>
-
-
 
                 </section>
             </div >
