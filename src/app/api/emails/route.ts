3856b49690786d4b26d23c85ea0370442ba8b360
Pedro Marques <pedromarques391@gmail.com>
2025-08-03 21:04:50 -0300

    feat: create payment router.

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { sendEmail } from './controller/mail.controller';
+import { sendEmail } from '../controller/mail.controller';
 
 export async function POST(req: NextRequest) {
     try {
---
+++ src/app/api/payment/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { payment } from "../controller/payment.controller";
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+
+        // console.log(body)
+
+        const res = await payment(body);
+
+        return NextResponse.json({ status: 200, data: res });
+
+    } catch (error) {
+        console.error('Erro ao processar pagamento:', error);
+        return NextResponse.json(
+            { status: 500 }
+        );
+    }
+}
