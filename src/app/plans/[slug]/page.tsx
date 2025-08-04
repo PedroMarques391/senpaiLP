23bc831ef515c854e162c99f1c6d2febcce56a75
Pedro Marques <pedromarques391@gmail.com>
2025-08-03 21:00:37 -0300

    feat: finalize Plans page.

--- src/app/plans/[slug]/page.tsx
+++ src/app/plans/[slug]/page.tsx
@@ -1,12 +1,14 @@
+
 import Layout from '@/src/components/layout/Layout';
 import { services } from '@/src/data'
 import { notFound } from 'next/navigation';
 import React from 'react'
 
+import PaymentButton from '@/src/components/shared/PaymentButton';
 
-export async function generateStaticParams(): Promise<{
-    slug: string;
-}[]> {
+
+
+export async function generateStaticParams(): Promise<{ slug: string }[]> {
     return services.map((service) => ({
         slug: service.url
     }))
@@ -14,15 +16,11 @@ export async function generateStaticParams(): Promise<{
 
 export default async function Page({
     params,
-}: {
-    params: Promise<{ slug: string }>
-
-}): Promise<React.JSX.Element> {
+}: { params: Promise<{ slug: string }> }): Promise<React.JSX.Element> {
     const { slug } = await params;
 
     const service = services.find(service => service.url === slug) ?? notFound();
 
-    console.log(service)
 
     return (
         <Layout
@@ -30,8 +28,26 @@ export default async function Page({
             backgroundHeader='bg-black'
             load={false}
         >
-            <main className='w-full min-h-screen'>
-                <p>ou</p>
+            <main className='w-full min-h-screen mt-22'>
+                <section className="w-full max-w-3xl mx-auto flex flex-col items-center text- py-10">
+                    <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
+                        {service.service}
+                    </h1>
+                    <p className="text-2xl md:text-3xl font-semibold text-green-600 mb-6 font-sans">
+                        R$ {service.value} / mês
+                    </p>
+                    <p className="text-base md:text-lg text-gray-600 mb-8">
+                        {service.description}
+                    </p>
+                    <p>form</p>
+
+                    <PaymentButton
+                        title={service.service}
+                        price={service.value}
+                        text={service.textButton}
+                    />
+                </section>
+
             </main>
         </Layout>
     );
