9cb03e60dca466e128a5e365d3ccdb1d381a770f
Pedro Marques <pedromarques391@gmail.com>
2025-08-11 16:54:21 -0300

    refactor: remove loading screen from first loading.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react'
 import Hero from './Hero'
 import { Footer } from './Footer'
-import { LoadingPage } from '@/src/components/shared/LoadingPage'
 import { Headset } from 'lucide-react'
 import { Header } from './Header'
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog'
@@ -26,9 +25,7 @@ const Layout = ({
     backgroundColor = "bg-white",
     backgroundHeader = "bg-transparent",
     container = true,
-    load = true
 }: ILayoutProps): React.JSX.Element => {
-    const [loading, setLoading] = useState<boolean>(false);
     const [showButton, setShowButton] = useState<boolean>(false);
 
     const methods = useEmailForm()
@@ -44,71 +41,67 @@ const Layout = ({
         return () => window.removeEventListener("scroll", handleScroll);
     }, [])
     return (
-        <>
-            {loading ?
-                <LoadingPage />
-                : (
-                    <main className={`w-full min-h-screen ${backgroundColor} relative`}>
-                        {hero ? <Hero /> : <Header background={backgroundHeader} />}
-                        <section className={`${container && "container"}  mx-auto  overflow-hidden `}>
-                            {children}
-                        </section>
-                        <Footer />
-                        <Dialog onOpenChange={(isOpen) => setShowButton(!isOpen)} >
-                            <DialogTrigger asChild>
-                                <button
-                                    aria-label='call support'
-                                    className={`bg-white/80 p-2 flex flex-col bottom-10 right-5 lg:right-12 fixed h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 rounded-2xl items-center justify-center shadow-lg cursor-pointer hover:shadow-xl transition-opacity duration-500 z-30 ${showButton ? "opacity-100" : "opacity-0"}`} >
-                                    <Headset className="h-6 w-6 md:h-8 md:w-8 mb-1" />
-                                    <span className="text-xs md:text-sm ">Suporte</span>
-                                </button>
-                            </DialogTrigger>
-                            <DialogContent
-                                className="sm:max-w-[425px]">
-                                <DialogHeader>
-                                    <DialogTitle>
-                                        Suporte ao Cliente
-                                    </DialogTitle>
-                                    <DialogDescription>
-                                        Online todos os dias, 24h por dia, sempre que precisar
-                                    </DialogDescription>
-                                </DialogHeader>
-                                <FormProvider {...methods} >
-                                    <FormDialog >
-                                        <FormField
-                                            label="Nome"
-                                            name="name"
-                                        />
 
-                                        <FormField
-                                            label="E-mail"
-                                            name="email"
-                                            type="email"
-                                        />
 
-                                        <FormField
-                                            label="Telefone"
-                                            name="phone"
-                                            maxLength={15}
-                                        />
+        <main className={`w-full min-h-screen ${backgroundColor} relative`}>
+            {hero ? <Hero /> : <Header background={backgroundHeader} />}
+            <section className={`${container && "container"}  mx-auto  overflow-hidden `}>
+                {children}
+            </section>
+            <Footer />
+            <Dialog onOpenChange={(isOpen) => setShowButton(!isOpen)} >
+                <DialogTrigger asChild>
+                    <button
+                        aria-label='call support'
+                        className={`bg-white/80 p-2 flex flex-col bottom-10 right-5 lg:right-12 fixed h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 rounded-2xl items-center justify-center shadow-lg cursor-pointer hover:shadow-xl transition-opacity duration-500 z-30 ${showButton ? "opacity-100" : "opacity-0"}`} >
+                        <Headset className="h-6 w-6 md:h-8 md:w-8 mb-1" />
+                        <span className="text-xs md:text-sm ">Suporte</span>
+                    </button>
+                </DialogTrigger>
+                <DialogContent
+                    className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>
+                            Suporte ao Cliente
+                        </DialogTitle>
+                        <DialogDescription>
+                            Online todos os dias, 24h por dia, sempre que precisar
+                        </DialogDescription>
+                    </DialogHeader>
+                    <FormProvider {...methods} >
+                        <FormDialog >
+                            <FormField
+                                label="Nome"
+                                name="name"
+                            />
 
-                                        <FormField
-                                            label="Assunto"
-                                            name="subject"
-                                        />
+                            <FormField
+                                label="E-mail"
+                                name="email"
+                                type="email"
+                            />
 
-                                        <FormField
-                                            label="Mensagem"
-                                            name="message"
-                                            type="textarea"
-                                        />
-                                    </FormDialog>
-                                </FormProvider>
-                            </DialogContent>
-                        </Dialog>
-                    </main>
-                )}
-        </>
+                            <FormField
+                                label="Telefone"
+                                name="phone"
+                                maxLength={15}
+                            />
+
+                            <FormField
+                                label="Assunto"
+                                name="subject"
+                            />
+
+                            <FormField
+                                label="Mensagem"
+                                name="message"
+                                type="textarea"
+                            />
+                        </FormDialog>
+                    </FormProvider>
+                </DialogContent>
+            </Dialog>
+        </main>
     )
 }
 
