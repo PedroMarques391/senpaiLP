b9fc25aa4aed3863161081478da40b38ff32a7d6
Pedro Marques <pedromarques391@gmail.com>
2025-08-11 18:55:29 -0300

    temp: temporary modifications.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Logo from "@/src/components/shared/Logo"
 import MenuDesktop from "@/src/components/menu/MenuDesktop"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Mobile from "@/src/components/menu/Mobile";
 import { usePathname } from "next/navigation";
 
@@ -10,9 +10,10 @@ type IHeaderProps = { background?: string }
 
 export const Header = ({ background }: IHeaderProps): React.JSX.Element => {
     const path = usePathname()
+    console.log(background)
 
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [scrolled, setScrolled] = useState(false);
+    // const [scrolled, setScrolled] = useState(false);
 
     function handleHref(href: string): string {
         const isHomePage: boolean = path === "/"
@@ -25,19 +26,18 @@ export const Header = ({ background }: IHeaderProps): React.JSX.Element => {
         setIsOpen((prev) => !prev);
     }
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrolled(window.scrollY > 50);
-        }
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //         setScrolled(window.scrollY > 50);
+    //     }
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, [])
+    //     window.addEventListener("scroll", handleScroll);
+    //     return () => window.removeEventListener("scroll", handleScroll);
+    // }, [])
 
 
     return (
-        <header className={`text-white py-3 flex justify-center font-sans items-center fixed top-0 left-0 transition-colors duration-500 w-full z-30 ${scrolled ? "bg-black/80" : background
-            }`}>
+        <header className={`text-white py-3 flex justify-center font-sans items-center fixed top-0 left-0 transition-colors duration-500 w-full z-30 bg-black`}>
             <nav className="w-full max-w-6xl flex justify-between items-center md:mx-5 lg:mx-0 relative px-5 ">
                 <Logo />
                 <button aria-label="Abrir/fechar Menu" onClick={handleMenu} className="md:hidden w-8 h-8  z-20">
