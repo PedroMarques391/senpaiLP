2afde2d6edbfd8a6fbae9635ba6454f312fd4503
Pedro Marques <pedromarques391@gmail.com>
2025-08-13 22:00:45 -0300

    feat: create test API routes for payment.

--- src/app/api/check/route.ts
+++ src/app/api/check/route.ts
@@ -1,19 +1,19 @@
-// app/api/payment/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import { createPayment } from "../controller/payment.controller";
+import { checkStatus } from "../controller/payment.controller";
 
 
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
 
-        const res = await createPayment(body);
+        console.log(body)
+
+        const res = await checkStatus(body.id);
+
         return NextResponse.json({ data: res }, { status: 200 });
 
-
-
     } catch (error) {
-        console.error('Erro ao processar pagamento:', error);
+        console.error('Erro ao verificar status:', error);
 
         return NextResponse.json(
             { error: 'Erro interno do servidor ao processar o pagamento' },
