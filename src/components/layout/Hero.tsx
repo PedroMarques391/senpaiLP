e1607eed3ba00b1238be0b13c4bd32c2a8b1f1ab
Pedro Marques <pedromarques391@gmail.com>
2025-08-23 09:48:06 -0300

    feat: add phone image in the hero and adjusting with grid.

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = (): React.JSX.Element => {
     return (
         <section>
             <div
-                className="relative text-content-inverse bg-gradient-to-r from-fuchsia-300 via-purple-300 to-slate-50 bg-center pb-20  md:px-20 rounded-b-[60px] overflow-hidden">
+                className="relative text-content-inverse bg-gradient-to-r from-fuchsia-300 via-purple-300 to-slate-50 bg-center   md:px-20 rounded-b-[60px] overflow-hidden">
                 <Image
                     src="/images/senpai/hero.webp"
                     alt="Hero"
@@ -34,14 +34,14 @@ const Hero = (): React.JSX.Element => {
                 <div className="absolute inset-0 bg-black/50 rounded-b-[60px]"></div>
                 <section
                     id="home"
-                    className="container relative  gap-10 items-center mx-auto py-10 md:pt-32 overflow-hidden justify-center ">
-                    <div className="flex flex-col gap-6 z-10 px-5 md:px-0 xl:px-5 pt-16 lg:col-span-2 overflow-hidden w-full h-[700px]  justify-center items-center">
+                    className="container relative grid grid-cols-1 lg:grid-cols-2  mx-auto  overflow-hidden ">
+                    <div className="flex flex-col gap-6 z-10 px-5 md:px-0 xl:px-5 pt-32 overflow-hidden w-full justify-center items-center ">
                         <motion.h1
                             ref={ref}
                             initial={{ x: "100%" }}
                             animate={{ x: isInView ? 0 : "100%" }}
                             transition={{ type: "spring", stiffness: 100, damping: 25, duration: 0.8 }}
-                            className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-tight ">
+                            className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-tight text-center">
                             Sua Nova Amiga Virtual para Figurinhas no WhatsApp
                         </motion.h1>
                         <motion.h2
@@ -49,18 +49,18 @@ const Hero = (): React.JSX.Element => {
                             initial={{ x: "-100%" }}
                             animate={{ x: isInView ? 0 : "-100%" }}
                             transition={{ type: "spring", stiffness: 50, damping: 20, duration: 2 }}
-                            className="text-lg md:text-base lg:text-xl font-bold">
+                            className="text-lg md:text-base text-center lg:text-xl">
 
                             A <span className="font-semibold">Senpai</span> está sempre disponível para deixar suas conversas mais divertidas e criativas! Com ela, você pode criar figurinhas personalizadas, transformar imagens, GIFs e vídeos em figurinhas animadas e muito mais. Tudo de forma rápida, fácil e gratuita. A sua bot de figurinhas para WhatsApp que torna cada conversa única!
                         </motion.h2>
-                        <div className="flex flex-col md:flex-row gap-10">
+                        <div className="flex flex-col gap-10">
                             <Link
                                 href="https://wa.me/555497153068?text=Ol%C3%A1%20senpai"
                                 target="_blank"
                                 prefetch
                                 rel="noopener noreferrer"
                             >
-                                <button className="bg-[#25D366] text-content-inverse font-bold py-2 px-3 rounded-lg shadow-md transition flex items-center gap-4 w-full md:w-fit duration-300 sm:py-1 sm:px-2 sm:gap-2 md:py-2 md:px-3 md:gap-4 hover:bg-[#1DA851] hover:scale-105 hover:shadow-lg active:scale-95 justify-center">
+                                <button className="bg-[#25D366] text-content-inverse font-bold py-2 px-3 rounded-full transition flex items-center gap-4 w-full  duration-300 sm:py-1 sm:px-2 sm:gap-2 md:py-2 md:px-3 md:gap-4 hover:bg-[#1DA851] hover:scale-105 hover:shadow-lg active:scale-95 justify-center">
                                     <BsWhatsapp aria-label="Icone do WhatsApp" className="flex-shrink-0 w-5 h-5 md:w7 md:h-7" />
 
                                     <div className="text-left">
@@ -82,9 +82,20 @@ const Hero = (): React.JSX.Element => {
                         </div>
                     </div>
 
+                    <div className="flex justify-center items-start ">
+                        <Image
+                            src="/images/phone.png"
+                            alt="Mockup de celular"
+                            width={600}
+                            height={1200}
+                            className="w-full max-w-[500px] md:max-w-[550px] lg:max-w-[600px] h-auto -mt-20 lg:mt-0"
+                            priority
+                        />
+                    </div>
+
                 </section>
             </div >
-            <div className=" bg-surface-card px-6 md:px-10 lg:md:px-20 rounded-b-[60px] -mt-16" >
+            <div className=" bg-surface-card px-6 md:px-10 lg:px-20 rounded-b-[60px] -mt-16" >
                 <section
                     ref={refCard}
                     className="grid grid-cols-1 md:grid-cols-3  md:w-full max-w-7xl  mx-auto place-items-center content-center text-center pt-24 pb-14 gap-5 md:gap-4 overflow-hidden" >
