269814132eb86a89588889c068732848264ef708
Pedro Marques <pedromarques391@gmail.com>
2025-08-23 19:04:09 -0300

    feat: remove <p> with the description and leaving only the title in the hero.

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = (): React.JSX.Element => {
     return (
         <section>
             <div
-                className="relative text-content-inverse bg-gradient-to-r from-fuchsia-300 via-purple-300 to-slate-50 bg-center   md:px-20 rounded-b-[60px] overflow-hidden">
+                className="relative text-content-inverse bg-gradient-to-r from-fuchsia-300 via-purple-300 to-slate-50 bg-center   md:px-20 rounded-b-[60px] overflow-hidden z-20">
                 <Image
                     src="/images/senpai/hero.webp"
                     alt="Hero"
@@ -34,43 +34,36 @@ const Hero = (): React.JSX.Element => {
                 <div className="absolute inset-0 bg-black/50 rounded-b-[60px]"></div>
                 <section
                     id="home"
-                    className="container relative grid grid-cols-1 lg:grid-cols-2  mx-auto  overflow-hidden ">
-                    <div className="flex flex-col gap-6 z-10 px-5 md:px-0 xl:px-5 pt-32 overflow-hidden w-full justify-center items-center ">
+                    className="container relative grid grid-cols-1 lg:grid-cols-2  mx-auto  overflow-hidden">
+                    <div className="flex flex-col gap-6 px-5 md:px-0 xl:px-5 pt-32 overflow-hidden w-full justify-center  items-center lg:items-start lg:gap-9">
                         <motion.h1
                             ref={ref}
                             initial={{ x: "100%" }}
                             animate={{ x: isInView ? 0 : "100%" }}
                             transition={{ type: "spring", stiffness: 100, damping: 25, duration: 0.8 }}
-                            className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-tight text-center">
+                            className="text-3xl md:text-5xl md:w-[80%] font-bold leading-tight text-center lg:text-left"
+                        >
                             Sua Nova Amiga Virtual para Figurinhas no WhatsApp
                         </motion.h1>
-                        <motion.h2
-                            ref={ref}
-                            initial={{ x: "-100%" }}
-                            animate={{ x: isInView ? 0 : "-100%" }}
-                            transition={{ type: "spring", stiffness: 50, damping: 20, duration: 2 }}
-                            className="text-lg md:text-base text-center lg:text-xl">
 
-                            A <span className="font-semibold">Senpai</span> está sempre disponível para deixar suas conversas mais divertidas e criativas! Com ela, você pode criar figurinhas personalizadas, transformar imagens, GIFs e vídeos em figurinhas animadas e muito mais. Tudo de forma rápida, fácil e gratuita. A sua bot de figurinhas para WhatsApp que torna cada conversa única!
-                        </motion.h2>
-                        <div className="flex flex-col gap-10">
+                        <div className="flex flex-col gap-10 md:gap-5 w-full mb-3">
                             <Link
                                 href="https://wa.me/555497153068?text=Ol%C3%A1%20senpai"
                                 target="_blank"
-                                prefetch
                                 rel="noopener noreferrer"
                             >
-                                <button className="bg-[#25D366] text-content-inverse font-bold py-2 px-3 rounded-full transition flex items-center gap-4 w-full  duration-300 sm:py-1 sm:px-2 sm:gap-2 md:py-2 md:px-3 md:gap-4 hover:bg-[#1DA851] hover:scale-105 hover:shadow-lg active:scale-95 justify-center">
-                                    <BsWhatsapp aria-label="Icone do WhatsApp" className="flex-shrink-0 w-5 h-5 md:w7 md:h-7" />
-
-                                    <div className="text-left">
-                                        <p className="text-xs">Começar no</p>
-                                        <p className="text-base font-semibolda lg:text-[17px]">WhatsApp</p>
-                                    </div>
+                                <button
+                                    className="flex items-center justify-center gap-3  mx-auto lg:mx-0  rounded-full bg-[#25D366] p-4 font-bold text-white  transition duration-300  hover:bg-[#1DA851] "
+                                >
+                                    <BsWhatsapp
+                                        aria-label="Ícone do WhatsApp"
+                                        className="w-6 h-6 flex-shrink-0"
+                                    />
+                                    Começar no WhatsApp
                                 </button>
-
                             </Link>
-                            <p className="text-content-inverse font-bold flex items-center justify-center md:justify-normal text-sm sm:text-xs md:text-sm gap-1 sm:gap-0 flex-wrap ">
+
+                            <p className="text-content-inverse font-bold flex text-sm sm:text-xs md:text-sm gap-1 sm:gap-0 flex-wrap  w-fit mx-auto lg:mx-0">
                                 Ao clicar no botão, você concorda com os&nbsp;
                                 <Link href="/termo-de-uso" prefetch target="_blank" className="text-[#25D366] font-semibold hover:underline whitespace-nowrap">
                                     Termos de Uso
@@ -82,13 +75,13 @@ const Hero = (): React.JSX.Element => {
                         </div>
                     </div>
 
-                    <div className="flex justify-center items-start ">
+                    <div className="flex justify-center items-end ">
                         <Image
                             src="/images/phone.png"
                             alt="Mockup de celular"
                             width={600}
                             height={1200}
-                            className="w-full max-w-[500px] md:max-w-[550px] lg:max-w-[600px] h-auto -mt-20 lg:mt-0"
+                            className="w-full max-w-[500px] md:max-w-[550px] lg:max-w-[600px] h-auto -mt-20 lg:mt-0 "
                             priority
                         />
                     </div>
