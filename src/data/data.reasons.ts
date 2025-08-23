285329da2d1112fec271352ae2df124160aa4a0a
Pedro Marques <pedromarques391@gmail.com>
2025-08-23 08:42:03 -0300

    feat: change text on the card of reasons.

--- src/data/data.reasons.ts
+++ src/data/data.reasons.ts
@@ -1,20 +1,20 @@
-import { DollarSign, Star, Users } from "lucide-react";
 import { IReasonsCard } from "../types";
+import { TrendingUp, Megaphone, Gift } from "lucide-react";
 
 export const reasonsCard: IReasonsCard[] = [
     {
-        icon: DollarSign,
+        icon: TrendingUp,
         title: 'Aumente sua receita',
         text: 'Ganhe comissões atrativas por cada cliente indicado ou venda realizada através da nossa parceria.'
     },
     {
-        icon: Star,
-        title: 'Acesso a recursos exclusivos',
-        text: 'Receba materiais de marketing, treinamentos especializados e suporte dedicado para impulsionar seus resultados.'
+        icon: Megaphone,
+        title: 'Aumente o público da sua marca',
+        text: 'Utilize nosso bot para atrair mais pessoas, engajar novos clientes e ampliar a presença da sua marca no digital.'
     },
     {
-        icon: Users,
-        title: 'Agregue valor ao seu portfólio',
-        text: 'Ofereça nosso bot aos seus clientes e fortaleça seu portfólio com uma solução líder de mercado.'
+        icon: Gift,
+        title: 'Ofereça descontos exclusivos',
+        text: 'Proporcione vantagens especiais para seus clientes com cupons e condições exclusivas, gerando fidelização e mais vendas.'
     }
 ];
