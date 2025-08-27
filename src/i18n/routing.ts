0fc3cd6417514a51949dfe6013894f861494cbfb
Pedro Marques <pedromarques391@gmail.com>
2025-08-27 18:54:35 -0300

    feat: add folder "i18n" to configure routing, navigation and requests.

---
+++ src/i18n/routing.ts
@@ -0,0 +1,23 @@
+import { defineRouting } from 'next-intl/routing';
+
+export const routing = defineRouting({
+    locales: ['en', 'pt'],
+    defaultLocale: 'pt',
+
+    pathnames: {
+        '/about': {
+            en: '/about',
+            pt: '/sobre'
+        },
+        '/partners': {
+            en: '/partners',
+            pt: '/parceiros'
+        },
+        '/thank-you': {
+            en: '/thank-you',
+            pt: '/agradecimento'
+        }
+
+    },
+
+});
