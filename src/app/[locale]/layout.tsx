277d6d5a8625d89a1105dde202936a91730cf7ea
Pedro Marques <pedromarques391@gmail.com>
2025-08-28 23:38:05 -0300

    refactor: change metadata constant by generateMetadata function.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react"
 import { Roboto, Poppins } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { jsonLd } from "../../utils";
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/src/i18n/routing';
 import React from "react";
@@ -21,41 +21,47 @@ const cabin_Condensed = Roboto({
   weight: "400",
 });
 
-export const metadata: Metadata = {
-  title: 'BotDoSenpai - Crie Figurinhas',
-  description: 'A Senpai é o bot de figurinhas para WhatsApp mais querida do Brasil! Transforme imagens, GIFs e vídeos em figurinhas animadas em segundos. Tudo 100% gratuito, sem precisar baixar nada, e com recursos exclusivos para deixar suas conversas mais divertidas, criativas e personalizadas.',
-  metadataBase: new URL('https://botdosenpai.com.br'),
-  alternates: {
-    canonical: '/',
-  },
-  authors: [{ name: "Pedro Marques" }],
-  applicationName: "BotDoSenpai",
-  keywords: ["bot de figurinha", "WhatsApp", "figurinhas divertidas", "BotSenpai", "conversas animadas", "figurinhas", "senpai", "dosenpai"],
-  openGraph: {
-    title: "BotDoSenpai - Seu bot ideal",
-    description: "Transforme suas conversas no WhatsApp com um bot de figurinhas divertido!",
-    siteName: "BotDoSenpai",
-    url: "https://botdosenpai.com.br/",
-    type: "website",
-    locale: "pt_BR",
-    images: [
-      {
-        url: "https://botdosenpai.com.br/wallpaper.png",
-        width: 800,
-        height: 600,
-        alt: "BotDoSenpai - O bot de figurinha mais fofo da internet!",
-      }
-    ]
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Bot de Figurinha",
-    creator: "@BotDoSenpai",
-    description: "Transforme suas conversas no WhatsApp com um bot de figurinhas divertido!",
-    site: "@BotDoSenpai",
-    images: ["/wallpaper.png"]
-  },
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
+  const t = await getTranslations({ locale, namespace: 'metadata.home' });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    metadataBase: new URL('https://botdosenpai.com.br'),
+    alternates: {
+      canonical: '/',
+    },
+    authors: [{ name: "Pedro Marques" }],
+    applicationName: "BotDoSenpai",
+    keywords: t.raw("keywords"),
+  };
 }
+
+// openGraph: {
+//     title: "BotDoSenpai - Seu bot ideal",
+//     description: "Transforme suas conversas no WhatsApp com um bot de figurinhas divertido!",
+//     siteName: "BotDoSenpai",
+//     url: "https://botdosenpai.com.br/",
+//     type: "website",
+//     locale: "pt_BR",
+//     images: [
+//       {
+//         url: "https://botdosenpai.com.br/wallpaper.png",
+//         width: 800,
+//         height: 600,
+//         alt: "BotDoSenpai - O bot de figurinha mais fofo da internet!",
+//       }
+//     ]
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Bot de Figurinha",
+//     creator: "@BotDoSenpai",
+//     description: "Transforme suas conversas no WhatsApp com um bot de figurinhas divertido!",
+//     site: "@BotDoSenpai",
+//     images: ["/wallpaper.png"]
+//   },
+
 
 
 export default async function RootLayout({
