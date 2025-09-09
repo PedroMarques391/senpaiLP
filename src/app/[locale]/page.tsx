7f2a159bec81b661908e654d3c135f8aec9dd2b4
Pedro Marques <pedromarques391@gmail.com>
2025-09-09 19:55:52 -0300

    refactor:move all steps to dedicated component.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,7 @@ import CarouselBenefit from "@/src/components/sections/Carousel";
 import { Faq } from "@/src/components/sections/Faq";
 import FunctionsCard from "@/src/components/sections/FunctionsCard";
 import Info from "@/src/components/sections/Info";
-import PromoModal from "@/src/components/sections/PromoModal";
+import PromoFlowModal from "@/src/components/sections/PromoFlowModal";
 import Services from "@/src/components/sections/Services";
 import Title from "@/src/components/ui/title";
 import { functionsCardItens, services } from "@/src/data";
@@ -34,7 +34,7 @@ export default function Home() {
       <div className="ksksk" id="func">
         <Title>{t("home.question")}</Title>
       </div>
-      <PromoModal />
+      <PromoFlowModal />
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl w-[80%] sm:w-[60%] md:w-full mx-auto">
         {functionsCardItens.map((functionsCardItem, item) => (
           <FunctionsCard
---
+++ src/components/sections/PromoFlowModal.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { Dialog, DialogContent } from "../ui/dialog";
+import { ModalSteps } from "./ModalSteps";
+
+enum MODAL {
+  NONE = 0,
+  PROMO = 1,
+  FORM = 2,
+  PAYMENT = 3,
+  thankYou = 4,
+}
+
+export default function PromoFlowModal() {
+  const [pix, setPix] = useState<string>("");
+  const [steps, setSteps] = useState(MODAL.NONE);
+  const [countdown, setCountdown] = useState(300);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    const claimed = localStorage.getItem("hasClaimed");
+    if (!claimed) {
+      const timout = setTimeout(() => setSteps(MODAL.PROMO), 350);
+      return () => clearTimeout(timout);
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      new Promise((resolve) => setTimeout(resolve, 2000));
+      const pixKey = await fetch("/api/payment/pix", { method: "GET" });
+      return pixKey.json();
+    };
+    if (steps === MODAL.PAYMENT) {
+      fetchData().then((res) => {
+        setPix(res.key);
+      });
+      const interval = setInterval(() => {
+        setCountdown((prev) => {
+          if (prev <= 0) {
+            clearInterval(interval);
+            markAsClaimed();
+          }
+          return prev - 1;
+        });
+      }, 1000);
+      return () => clearTimeout(interval);
+    }
+    if (steps === MODAL.thankYou) {
+      const timer = setTimeout(() => {
+        markAsClaimed();
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [steps]);
+
+  function markAsClaimed() {
+    if (typeof window === "undefined") return;
+    try {
+      localStorage.setItem("hasClaimed", "true");
+    } catch (e) {
+      console.error("[PromoModal] Failed to set hasClaimed", e);
+    } finally {
+      setSteps(MODAL.NONE);
+    }
+  }
+
+  return (
+    <Dialog open={steps !== MODAL.NONE} onOpenChange={markAsClaimed}>
+      <DialogContent
+        onInteractOutside={(e) => e.preventDefault()}
+        onEscapeKeyDown={(e) => e.preventDefault()}
+        className="w-[min(92vw,640px)] max-w-lg rounded-3xl bg-gradient-to-br  from-pink-50 via-pink-100 to-purple-50 border-none"
+      >
+        {steps === MODAL.PROMO && (
+          <ModalSteps.PROMO handleNextStep={() => setSteps(MODAL.FORM)} onClose={markAsClaimed} />
+        )}
+        {steps === MODAL.FORM && <ModalSteps.FORM handleNextStep={() => setSteps(MODAL.PAYMENT)} />}
+        {steps === MODAL.PAYMENT && (
+          <ModalSteps.PAYMENT
+            handleNextStep={() => setSteps(MODAL.thankYou)}
+            pix={pix}
+            countdown={countdown}
+          />
+        )}
+        {steps === MODAL.thankYou && <ModalSteps.THANKYOU onClose={markAsClaimed} />}
+      </DialogContent>
+    </Dialog>
+  );
+}
