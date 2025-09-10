535625772ebb0ade47755df8e18cde62bac1f865
Pedro Marques <pedromarques391@gmail.com>
2025-09-10 10:33:44 -0300

    fix: move prop subject in requestData, removing from IProposalRequestData and add in IContactResquestData.

--- src/types/requestData.ts
+++ src/types/requestData.ts
@@ -1,16 +1,16 @@
 export interface IContactResquestData {
-    type: "support" | "partnership";
-    name: string;
-    email: string;
-    phone: string;
-    message?: string;
+  type: "support" | "partnership";
+  name: string;
+  email: string;
+  phone: string;
+  subject: string;
+  message?: string;
 }
 
 export interface IProposalRequestData extends IContactResquestData {
-    segment: string;
-    proposal: string;
-    publicInfo: string;
-    budgeting: string;
-    more: string;
-    subject: string;
+  segment: string;
+  proposal: string;
+  publicInfo: string;
+  budgeting: string;
+  more: string;
 }
