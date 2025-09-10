273b338c684bdb966ae243b29d3335e2260afae3
Pedro Marques <pedromarques391@gmail.com>
2025-09-10 10:37:33 -0300

    fix: fix error handling in emails/support, now the use have a visual response in case of failure.

--- src/app/api/emails/support/route.ts
+++ src/app/api/emails/support/route.ts
@@ -2,16 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { sendEmail } from "../../controller/mail.controller";
 
 export async function POST(req: NextRequest) {
-    const data = await req.json();
+  const data = await req.json();
 
-    try {
-        await sendEmail(data);
-
-        return NextResponse.json({ status: 200 });
-    } catch (error) {
-        console.error("Erro ao enviar email:", error);
-        return NextResponse.json(
-            { status: 500 }
-        );
-    }
+  try {
+    await sendEmail(data);
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.log(error);
+    NextResponse.json({ status: 500 });
+    throw new Error("[api/emails/support] Erro ao enviar email", { cause: error });
+  }
 }
