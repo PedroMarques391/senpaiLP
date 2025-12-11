916c81c37ee42ff5509e33c973cbf6a0e61452ab
Pedro Marques <pedromarques391@gmail.com>
2025-12-11 16:18:04 -0300

    feat: add new question and answer in the faq.

--- src/data/data.faq.ts
+++ src/data/data.faq.ts
@@ -1,11 +1,12 @@
 import { IFaq } from "../types";
 
 export const faq: IFaq[] = [
-    { id: 1, question: "q1.question", answer: "q1.answer" },
-    { id: 2, question: "q2.question", answer: "q2.answer" },
-    { id: 3, question: "q3.question", answer: "q3.answer" },
-    { id: 4, question: "q4.question", answer: "q4.answer" },
-    { id: 5, question: "q5.question", answer: "q5.answer" },
-    { id: 6, question: "q6.question", answer: "q6.answer" },
-    { id: 7, question: "q7.question", answer: "q7.answer" },
+  { id: 1, question: "q1.question", answer: "q1.answer" },
+  { id: 2, question: "q2.question", answer: "q2.answer" },
+  { id: 8, question: "q8.question", answer: "q8.answer" },
+  { id: 3, question: "q3.question", answer: "q3.answer" },
+  { id: 4, question: "q4.question", answer: "q4.answer" },
+  { id: 5, question: "q5.question", answer: "q5.answer" },
+  { id: 6, question: "q6.question", answer: "q6.answer" },
+  { id: 7, question: "q7.question", answer: "q7.answer" },
 ];
